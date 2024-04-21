ec2da07bf3cdf614e02053167970b56287c30b1f
KDWBadSinger <141695186+KDWBadSinger@users.noreply.github.com>
2024-04-21 15:50:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var API_key = "RGAPI-f133a35b-7687-4a49-9327-8c2c380dc07a";
+var API_key = "RGAPI-54b51f51-8f60-4f68-8522-97fd20de9721";
 var server_url= "";
 const Regions = [
     'br1.api.riotgames.com',
